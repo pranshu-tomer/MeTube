fffcc1bbf18cd559032aee64e89f0aead583b112
pranshu-tomer <tomerpranshu11@gmai.com>
2024-09-09 10:43:45 +0530

    add aggregation pipeline

--- README.md
+++ README.md
@@ -26,3 +26,32 @@
 17. Postman Collection
 18. register user and login with refreshtoken and access token
 19. === , ?
+
+20. Like we store user details in subscription model , so How we get subscriber details to user Account
+
+Answer is - MongoDB Aggregation Pipeline
+why not just add subscripber to user model using array - Answer is DSA (million data me se ek delete karo)
+
+Aggregation Pipeline
+An aggregation pipeline consists of one or more stages the process dicuments
+* Each stage performs an operation on input document
+* The document that are output from a stage are passed to next stage
+
+how to write
+model.aggregate([])
+array contain objects , each object act as stage
+
+$lookup : {
+    from :
+    localField:
+    foreignField:
+    as: 
+}
+
+$addFields: {
+    author_details: {
+        $first:"$as"
+    }
+}
+
+these pipeline returns array with objects
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -187,5 +187,76 @@ export const changeCurrentPassword = asyncHandler(async (req,res) => {
 export const getCurrentUser = asyncHandler(async(req,res) => {
     return res
     .status(200)
-    .json(200,req.user,"Current User fetched successfully")
+    .json(new apiResponse(200,req.user,"Current User fetched successfully"))
+})
+
+export const getUserChannelProfile = asyncHandler(async(req,res) => {
+    const {username} = req.params
+    if(!username?.trim()){
+        throw new apiError(400,"Username is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id,"$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new apiError(404,"Channel does not exists")
+    }
+
+    return res
+    .status(200)
+    .json(new apiResponse(
+        200,
+        channel[0],
+        "User Channel fetched successfully"
+    ))
 })
