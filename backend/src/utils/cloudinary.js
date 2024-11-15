3122be66c50b4c87582830eac1c5ae507e67bcb4
Pranshu Tomer <136837700+pranshu-tomer@users.noreply.github.com>
2024-11-16 00:23:39 +0530

    Update cloudinary.js

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -2,11 +2,7 @@ import { v2 as cloudinary } from 'cloudinary';
 import fs from 'fs'
 
 
-cloudinary.config({ 
-    cloud_name: 'deqewgc25', 
-    api_key: '665387174154743', 
-    api_secret: '92c6FqiAPEelQs7VmLfX0i_BpAg'
-})
+
 
 export const uploadCloudinary = async (localPath) => {
     try{
